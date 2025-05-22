c217d50a6ae3659e9a83c3b8973bfe2d7889c165
RAVI424983 <ravitejapaccha@gmail.com>
2025-05-22 12:18:56 +0530

    adding functions

--- functions.sh
+++ functions.sh
@@ -9,3 +9,25 @@ exit 1
 else
 echo "your using root access" 
 fi
+
+dnf list installed nginx
+if [ $? -ne 0]
+then
+echo "nginx is not installed "
+dnf install nginx
+validate $? "nginx"
+else
+echo"nginx is already installed"
+fi
+
+validate ()
+{
+    if [ $1 -ne 0 ]
+    then
+     echo " $2 is not installed "
+     exit 1
+   else
+   echo "$2 is successfully installed"
+   fi
+
+}
