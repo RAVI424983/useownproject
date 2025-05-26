4d833e96b133859eba4bd42fbbb915d282fd6e39
RAVI424983 <ravitejapaccha@gmail.com>
2025-05-26 22:20:38 +0530

    updated disk

--- roboshopscripting/diskspace.sh
+++ roboshopscripting/diskspace.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-diskusage =$(df -ht | awk '{print $6F}' | cut -d "%" -f1)
+diskusage =$(df -hT | awk '{print $6F}' | cut -d "%" -f1)
 echo "disk usage of the system is : $diskusage"
 
 
