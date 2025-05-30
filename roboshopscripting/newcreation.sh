8bc774ace2e2b925283966d250a89d9624d1edb7
RAVI424983 <ravitejapaccha@gmail.com>
2025-05-30 11:45:09 +0530

    new creations

--- roboshopscripting/newcreation.sh
+++ roboshopscripting/newcreation.sh
@@ -6,8 +6,8 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 ZONE_ID="Z06227262ICYSRHLV5M7R" # replace with your ZONE ID
 DOMAIN_NAME="ravidevops.site" # replace with your domain
 
-for instance in ${INSTANCES[@]}
-#for instance in $@
+#for instance in ${INSTANCES[@]}
+for instance in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0fdd8ce59d7a8ef82 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
